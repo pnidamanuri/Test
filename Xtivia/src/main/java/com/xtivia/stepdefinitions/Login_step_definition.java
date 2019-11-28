8c42d337fafa4af7cc826e15a18fa357b7ad2174
pnidamanuri <pnidamanuri@XTV-NIDAMANURIP.HYDUS.COM>
2019-11-28 12:34:18 +0530

    First Commit

---
+++ Xtivia/src/main/java/com/xtivia/stepdefinitions/Login_step_definition.java
@@ -0,0 +1,78 @@
+package com.xtivia.stepdefinitions;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.xtivia.pageobjects.HomePage;
+
+import org.openqa.selenium.WebElement;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+	public class Login_step_definition{
+
+		Logger APPLICATION_LOGS = Logger.getLogger("devpinoyLogger");
+		static final String path = "D:\\Poorna Bkup\\Poorna\\Poorna Backup\\Softwares\\log4j\\log4j.properties";
+		
+	
+		public HomePage homePage= new HomePage();
+		
+		public WebDriver driver;
+		
+		@Given("^user on home page$")
+	    public void user_on_home_page() throws Throwable {
+//			System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\geckodriver-v0.11.1-win64\\geckodriver.exe");
+//		    driver = new FirefoxDriver();
+		     
+		    System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		    driver = new ChromeDriver();
+		    driver.manage().deleteAllCookies();
+		    driver.manage().window().maximize();
+		    driver.navigate().refresh();
+		    driver.manage().deleteAllCookies();
+          	driver.get("https://www.flipkart.com/");
+          	
+			APPLICATION_LOGS.debug("Application launched in Chrome browser");
+	    }
+		
+		
+		 @When("^Login to the application$")
+		    public void login_to_the_application() throws Throwable {
+		
+			homePage=PageFactory.initElements(driver, HomePage.class);
+				homePage.logindetails("9676609052", "Poorna@1");
+		    }
+		 
+		 @Then("^I go to Iphone store$")
+		    public void i_go_to_iphone_store() throws Throwable {
+			 homePage=PageFactory.initElements(driver, HomePage.class);
+				Thread.sleep(1000);
+				 homePage.Ele.click();
+				 Thread.sleep(1000);
+				 homePage.Apple.click();
+		    }
+		 
+		 @And("^I navigate to Samsung store and select Sansung J8$")
+		    public void i_navigate_to_samsung_store_and_select_sansung_j8() throws Throwable {
+			 homePage=PageFactory.initElements(driver, HomePage.class);
+				Thread.sleep(1000);
+				 homePage.Ele.click();
+				 Thread.sleep(1000);
+				 homePage.Samsung.click();
+				 Thread.sleep(1000);
+				 homePage.SamsungJ8.click();
+		    }
+		 
+		 		 
+		 @And("^I quit broswer$")
+		    public void i_quit_broswer() throws Throwable {
+		        driver.quit();
+		    }
+
+	
+	}		
---
+++ Xtivia/src/main/java/com/xtivia/stepdefinitions/Runner.java
@@ -0,0 +1,20 @@
+package com.xtivia.stepdefinitions;
+
+import org.junit.runner.RunWith;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+
+@CucumberOptions(features ="src/main/java/com/xtivia/stepdefinitions/login.feature", 
+
+//format = {"pretty","html:/Users/pnidamanuri/workspace/Xtivia/reports"})
+plugin = {
+	"pretty", "json:reports/report.json", "junit:reports/report.xml", "html:reports/html" })
+
+public class Runner { 
+}
+
